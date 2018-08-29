7e630d31498cc2ba6063b54b896bdb8864111a48
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-08-29 10:04:32 +0200

    - UDPWriter: use a centralized 'send' function - UDPWriter: use a single socket when multiple calls are fired

--- src/UDPWriter.php
+++ src/UDPWriter.php
@@ -20,6 +20,59 @@ class UDPWriter implements WriterInterface
         $this->port = $port;
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function write(string $content, bool $flush = true): void
+    {
+        $this->enqueue($content);
+
+        if (!$flush) {
+            return;
+        }
+
+        $this->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function flush(): void
+    {
+        $buffer = $this->getEnqueuedMessagesBuffer();
+        $socket = null;
+
+        $bufferChunks = array_chunk($buffer, 128);
+
+        // Only one message/chunk to send
+        if (\count($bufferChunks) === 1 && \count($bufferChunks[0]) === 1) {
+            $this->send($socket, $bufferChunks[0][0]);
+            $this->queue = [];
+
+            return;
+        }
+
+        foreach ($bufferChunks as $buffer) {
+            try {
+                $messageId = \random_bytes(8);
+            } catch (\Exception $exception) {
+                throw new GELFException('Impossible to gather enough entropy', GELFException::CODE_CANT_SEND_MESSAGE);
+            }
+
+            $chunkSize = \count($buffer);
+            $header = "\x1e\x0f" . $messageId;
+            $packedChunkSize = pack('C', $chunkSize);
+
+            foreach ($buffer as $i => $part) {
+                $message = $header . pack('C', $i) . $packedChunkSize . $part;
+
+                $socket = $this->send($socket, $message);
+            }
+        }
+
+        $this->queue = [];
+    }
+
     protected function enqueue(string $content): void
     {
         if (!$this->queue) {
@@ -30,34 +83,9 @@ class UDPWriter implements WriterInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @return string[]
      */
-    public function write(string $content, bool $flush = true): void
-    {
-        if (!$flush) {
-            $this->enqueue($content);
-
-            return;
-        }
-
-        $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-
-        if ($socket === false) {
-            throw new GELFException('Impossible to open a UDP socket', GELFException::CODE_CANT_SEND_MESSAGE);
-        }
-
-        $contentLen = strlen($content);
-        $bytesSent = socket_sendto($socket, $content, $contentLen, 0, $this->address, $this->port);
-
-        if ($bytesSent !== $contentLen) {
-            throw new GELFException(
-                sprintf('Sent %d bytes, %d bytes expected', $bytesSent, $contentLen),
-                GELFException::CODE_CANT_SEND_MESSAGE
-            );
-        }
-    }
-
-    private function getEnqueuedMessagesBuffer(): array
+    protected function getEnqueuedMessagesBuffer(): array
     {
         $buffer = [];
 
@@ -66,7 +94,7 @@ class UDPWriter implements WriterInterface
         $buffKey = 0;
 
         foreach ($this->queue as $message) {
-            foreach (str_split($message) as $char) {
+            foreach (\str_split($message) as $char) {
                 if ($buffSize === 8180) {
                     $buffSize = 0;
                     $buffer[$buffKey++] = $lastBuff;
@@ -84,40 +112,32 @@ class UDPWriter implements WriterInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @param resource $socket
+     * @param string   $content
+     *
+     * @return resource the socket
+     * @throws GELFException
      */
-    public function flush(): void
+    private function send($socket, string $content)
     {
-        $buffer = $this->getEnqueuedMessagesBuffer();
+        if ($socket === null) {
+            $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
-        if (\count($buffer) === 1) {
-            if ($buffer[0]) {
-                $this->write($buffer[0]);
-            }
-            $this->queue = [];
-
-            return;
-        }
-
-        $bufferChunks = array_chunk($buffer, 128);
-        foreach ($bufferChunks as $buffer) {
-            try {
-                $messageId = \random_bytes(8);
-            } catch (\Exception $exception) {
-                throw new GELFException('Impossible to gather enough entropy', GELFException::CODE_CANT_SEND_MESSAGE);
-            }
-
-            $chunkSize = \count($buffer);
-            $header = "\x1e\x0f" . $messageId;
-            $packedChunkSize = pack('C', $chunkSize);
-
-            foreach ($buffer as $i => $part) {
-                $message = $header . pack('C', $i) . $packedChunkSize . $part;
-
-                $this->write($message);
+            if ($socket === false) {
+                throw new GELFException('Impossible to open a UDP socket', GELFException::CODE_CANT_SEND_MESSAGE);
             }
         }
 
-        $this->queue = [];
+        $contentLen = \strlen($content);
+        $bytesSent = socket_sendto($socket, $content, $contentLen, 0, $this->address, $this->port);
+
+        if ($bytesSent !== $contentLen) {
+            throw new GELFException(
+                sprintf('Sent %d bytes, %d bytes expected', $bytesSent, $contentLen),
+                GELFException::CODE_CANT_SEND_MESSAGE
+            );
+        }
+
+        return $socket;
     }
 }
